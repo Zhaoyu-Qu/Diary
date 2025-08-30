fd4f4b50bfb7e4c925486ad729dc5e2526af4c66
Jason Qu <Jason.Qu+cba@cba.com.au>
2025-08-30 21:36:50 +1000

    create initial projects and set up entity models

---
+++ Diary.DataContext/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Diary.DataContext;
+
+public class Class1
+{
+
+}
---
+++ Diary.EntityModels/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace Diary.EntityModels;
+
+public class Note
+{
+    public int Id { get; set; } // Primary key
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+}
